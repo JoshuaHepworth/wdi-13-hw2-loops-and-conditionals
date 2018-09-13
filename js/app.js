712111b67a9011ba189842af076aa7770612115a
Josh Hepworth <jrhepworth12@gmail.com>
2018-09-12 21:30:30 -0500

    Commit 5 - Getting to Know You answered

--- js/app.js
+++ js/app.js
@@ -10,8 +10,9 @@ for(let i = 1; i <= 20; i++  ) {
 
 //2. Write a for loop that will log only the even numbers in 0 through 200.
 
-
+//start at 0/while i less than equal to 200/add one
 for(let i = 0; i <= 200; i++) {
+	//if the value of i is divisible by 2 its even
 	if (i % 2 === 0) {
 		console.log(i);
 	}
@@ -19,19 +20,24 @@ for(let i = 0; i <= 200; i++) {
 }
 
 //3. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
-// let words = "...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away...";
+// let words = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+// let random = words[Math.floor(2*Math.random())]
+let words = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+var randomValue = words[Math.floor(words.length * Math.random())];
+console.log(randomValue);
 
-// for(let i = 0; i < 20; i++) {
-// 	if (i % 2 === 0){
-// 		console.log(Math.floor(Math.random() * 20))
-// 	}
-// 	console.log('Love me, pet me! HSSSSSSS!');
-// }
+for(let i = 0; i < 20; i++) {
+	if (i % 2 === 0){
+		console.log('Love me, pet me! HSSSSSSS!');
+	}
+// console.log('Love me, pet me! HSSSSSSS!');
+}
 
 
 
 
-//FIZZBUZZ
+
+//4. FIZZBUZZ
 
   let result;
   //declaring variable for highest number
@@ -52,6 +58,81 @@ for(let i = 0; i <= 200; i++) {
 result = i; }
     console.log(result);
   }
+
+
+//5. GETTING TO KNOW YOU
+
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+
+//a. Daniel decides that Shahzad can't be named "Shahzad" anymore. Remove "Shahzad" from the shahzad array and replace it with "Gameboy".
+//splice removes first index replaces with new value
+shahzad.splice(0,1,"Gameboy");
+console.log(shahzad);
+
+// b. Daniel just had his birthday; change Daniel's array to reflect him being a year older. Don't just hard code 187--pretend that you don't know that his age is 186, and write your code to just make him a year older than whatever age he was.
+for (let i = 0; i < daniel.length; i++) {
+	console.log(daniel[i] += 1)
+}
+// console.log(daniel[1]+ 1);
+
+//c. Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+
+ryan.splice(2,2,"Gotham City");
+console.log(ryan)
+
+// d. Reuben left Durham 5 years ago to come to Chicago. Remove "Durham" from Reuben's array and add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
+
+reuben.pop([3]);
+reuben.push("Chicago");
+console.log(reuben);
+
+//e. Jim could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but see if you can do it in one line of code
+
+jim.pop();
+jim.push('Chicago', 'Denver', 'Smallville');
+console.log(jim);
+
+//f.Bonus: Whoops! Jim is actually only allowed to be in one of two cities. Whatever the first of the 3 cities for Jim is now, remove it from the array using .splice()
+
+jim.splice(0,0,0)
+console.log(jim);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
